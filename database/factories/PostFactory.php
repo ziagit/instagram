ef946bac444dedd91b474bb8750187e9b7fae7ae
esmatullah-hassani <esmatullah.hasani6067@gmail.com>
2020-12-08 10:39:34 +0430

    Post rundom image done

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,6 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Post::class, function (Faker $faker) {
     return [
         'image' => 'images/posts/post-image'.rand(1, 330).'.jpg',
-        'description' => "Post ".rand(1,7),
+        'description' => "This is the post ".rand(1,330),
     ];
 });
